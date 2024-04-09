db4dffef55bee42642b9d084547525dbe218289b
kanishq_77 <ashakanishq77@gmail.com>
2024-04-09 08:58:05 -0700

    first

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 // db.js
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 const ConnectToMongo = async () => {
 	try {
-		await mongoose.connect(
-			"mongodb+srv://ashakanishq77:yxT0HdFDDJIiwZhg@cluster0.psdnbj1.mongodb.net/inotes?retryWrites=true&w=majority&appName=Cluster0",
-			{
-				useNewUrlParser: true,
-				// useCreateIndex: true,
-				useUnifiedTopology: true,
-				// useFindAndModify: false,
-			}
-		);
+		await mongoose.connect(MONGO_URL, {
+			useNewUrlParser: true,
+			// useCreateIndex: true,
+			useUnifiedTopology: true,
+			// useFindAndModify: false,
+		});
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error);
--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "I'm kanishq";
+const JWT_SECRET = process.env.JWT_SECRET;
 const fetchuser = (req, res, next) => {
 	const token = req.header("auth-token");
 	if (!token) {
