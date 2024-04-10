2f7b3a5ce38f3231bda95b8c4af49423ed4dce25
kanishq_77 <ashakanishq77@gmail.com>
2024-04-10 03:14:30 -0700

    cors

--- index.js
+++ index.js
@@ -9,23 +9,12 @@ var cors = require("cors");
 app.use(express.json());
 // available routes
 const corsOptions = {
-	origin: "http://localhost:3000",
+	origin: "https://inotes-amber.vercel.app",
 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-const allowedOrigins = ["https://inotes-amber.vercel.app"];
-app.use(
-	cors(corsOptions, {
-		origin: function (origin, callback) {
-			if (!origin || allowedOrigins.includes(origin)) {
-				callback(null, true);
-			} else {
-				callback(new Error("Not allowed by CORS"));
-			}
-		},
-		credentials: true,
-	})
-);
+// const allowedOrigins = ["https://inotes-amber.vercel.app"];
+app.use(cors(corsOptions));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
